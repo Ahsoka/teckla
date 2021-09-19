590900759e36ef81be32dd1b62de58f4463f5b58
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-19 15:35:06 -0700

    Created teckla/server.py

---
+++ teckla/server.py
@@ -0,0 +1,14 @@
+import aiohttp.web
+
+app = aiohttp.web.Application()
+routes = aiohttp.web.RouteTableDef()
+
+async def start_server():
+    runner = aiohttp.web.AppRunner(app)
+    await runner.setup()
+    site = aiohttp.web.TCPSite(runner, 'localhost')
+    await site.start()
+
+@routes.get('/')
+async def get_creds(request: aiohttp.web.Request):
+    print(f"{request=}")
