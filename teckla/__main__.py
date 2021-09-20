133d5611e1430485fd62f0da3d67aa05e7263c23
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-20 00:59:40 -0700

    Create tables in __main__.py

--- teckla/__main__.py
+++ teckla/__main__.py
@@ -1,6 +1,8 @@
 from .server import start_server
 from dotenv import load_dotenv
-from . import bot
+from sqlalchemy import text
+from .tables import mapper
+from . import bot, engine
 
 import os
 
@@ -8,6 +10,10 @@ load_dotenv()
 
 @bot.event
 async def on_ready():
+    async with engine.begin() as conn:
+        await conn.execute(text('PRAGMA foreign_keys=ON'))
+        await conn.run_sync(mapper.metadata.create_all)
+
     print(f"{bot.user} is ready to roll.")
 
 def main():
