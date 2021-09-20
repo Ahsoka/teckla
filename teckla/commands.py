81792c04280836cf0e3ab232f86ca397b1512189
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-20 01:30:05 -0700

    Fixed incorrect comparison in the `authenticate` command

--- teckla/commands.py
+++ teckla/commands.py
@@ -29,7 +29,7 @@ class CommandsCog(Cog):
         async with AsyncSession(engine) as sess:
             token: Token = await sess.get(Token, ctx.author.id)
 
-        if token and token.expiry < datetime.today():
+        if token and token.expiry > datetime.today():
             await ctx.send('You are already authenticated!', hidden=True)
         else:
             if aio_google.oauth2.is_ready(client_creds):
