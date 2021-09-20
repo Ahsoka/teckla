0531280297b12a7439424a3bfaaf2b9a3dd2b542
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-19 19:45:31 -0700

    Create main entry poing to aiogoogle

--- teckla/__init__.py
+++ teckla/__init__.py
@@ -1,10 +1,18 @@
 from discord_slash import SlashCommand
 from discord.ext.commands import Bot
+from aiogoogle import Aiogoogle
 
+import json
 import discord
 
 bot = Bot(command_prefix='/', intents=discord.Intents(messages=True, guilds=True))
 slash = SlashCommand(bot)
+
+with open('credentials.json') as file:
+    client_creds = json.load(file)['web']
+    client_creds['scopes'] = ['https://www.googleapis.com/auth/drive']
+    client_creds['redirect_uri'] = client_creds['redirect_uris'][0]
+    aio_google = Aiogoogle(client_creds=client_creds)
 
 from .commands import CommandsCog
 
