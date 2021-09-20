1b55dd86af6cc93c4bfee48199a07c54c4403a57
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-20 00:59:20 -0700

    Created tables.py

---
+++ teckla/tables.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, BigInteger, String, DateTime, ForeignKey
+from sqlalchemy.orm.decl_api import registry
+from dataclasses import dataclass, field
+from sqlalchemy.orm import relationship
+from typing import List
+
+import datetime
+
+mapper = registry()
+
+
+@mapper.mapped
+@dataclass
+class Scope:
+    __tablename__ = 'scopes'
+
+    __sa_dataclass_metadata_key__ = 'sa'
+
+    id: int = field(metadata={'sa': Column(BigInteger, ForeignKey('tokens.id'), primary_key=True)})
+    scope: str = field(metadata={'sa': Column(String(200), nullable=False)})
+
+
+@mapper.mapped
+@dataclass
+class Token:
+    __tablename__ = 'tokens'
+
+    __sa_dataclass_metadata_key__ = 'sa'
+
+    id: int = field(metadata={'sa': Column(BigInteger, primary_key=True)})
+    token: str = field(metadata={'sa': Column(String(200), nullable=False)})
+    expiry: datetime.datetime = field(
+        metadata={'sa': Column(DateTime(timezone=True), nullable=False)}
+    )
+    refresh_token: str = field(default=None, metadata={'sa': Column(String(200))})
+
+    scopes: List[Scope] = field(
+        default_factory=list, metadata={'sa': relationship(Scope, lazy='selectin')}
+    )
