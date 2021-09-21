172c40ca740a0fc6a97facd48824621314b03fa0
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-20 20:49:05 -0700

    Handle already existing tokens in the database

--- teckla/server.py
+++ teckla/server.py
@@ -21,12 +21,18 @@ async def get_creds(request: aiohttp.web.Request):
         full_user_creds = await aio_google.oauth2.build_user_creds(request.query.get('code'))
         async with AsyncSession(engine) as sess, sess.begin():
             discord_id = states[state][0]
-            sess.add(Token(
-                id=discord_id,
-                token=full_user_creds['access_token'],
-                expiry=dateutil.parser.isoparse(full_user_creds['expires_at']),
-                scopes=list(map(lambda scope: Scope(discord_id, scope), full_user_creds['scopes']))
-            ))
+            access_token = full_user_creds['access_token']
+            expiry = dateutil.parser.isoparse(full_user_creds['expires_at'])
+            scopes = list(map(lambda scope: Scope(discord_id, scope), full_user_creds['scopes']))
+
+            token: Token = await sess.get(Token, discord_id)
+
+            if token:
+                token.token = access_token
+                token.expiry = expiry
+                token.scopes = scopes
+            else:
+                sess.add(Token(id=discord_id, token=access_token, expiry=expiry, scopes=scopes))
         states[state][1].set()
     return aiohttp.web.Response(text="Hello world! 😉")
 
