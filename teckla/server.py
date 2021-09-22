82ac6adeb6cb861eaf0ae09b5ec8477079285884
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-09-22 01:42:38 -0700

    Fixed typo in server response when authentication fails

--- teckla/server.py
+++ teckla/server.py
@@ -50,6 +50,6 @@ async def get_creds(request: aiohttp.web.Request):
         return aiohttp.web.Response(text='Congratulations, you have successfully authenticated! You may close this window.')
     else:
         logger.debug('Received invalid request.')
-        return aiohttp.web.Response(body="Authentication failed, please using the /authenticate register command again.", status=401)
+        return aiohttp.web.Response(body="Authentication failed, please use the /authenticate register command again.", status=401)
 
 app.add_routes(routes)
